3e73d98a1fde3300aaac4e6992377b93a4305d55
JakubPatrik <jakub.pa27@gmail.com>
2020-10-31 20:43:47 +0100

    initial commit

---
+++ android/app/src/main/java/com/example/battery_plugin/MainActivity.java
@@ -0,0 +1,157 @@
+package com.example.battery_plugin;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
+import android.os.Bundle;
+import android.service.notification.StatusBarNotification;
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
+//
+import java.util.ArrayList;
+
+public class MainActivity extends FlutterActivity {
+  private static final String CHANNEL = "battery_plugin/channel";
+  private static final String CHANNEL_ID = "1010";
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    createNotificationChannel();
+  }
+
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    super.configureFlutterEngine(flutterEngine);
+    new MethodChannel(
+      flutterEngine.getDartExecutor().getBinaryMessenger(),
+      CHANNEL
+    )
+    .setMethodCallHandler(
+        (call, result) -> {
+          if (call.method.equals("getBatteryLevel")) {
+            int batteryLevel = getBatteryLevel();
+
+            if (batteryLevel != -1) {
+              result.success(batteryLevel);
+            } else {
+              result.error("UNAVAILABLE", "Battery level not available.", null);
+            }
+          } else if (call.method.equals("showNotification")) {
+            showNotification(
+              call.argument("title"),
+              call.argument("description"),
+              call.argument("id")
+            );
+          } else if (call.method.equals("readNotification")) {
+            ArrayList<String> res = readNotification();
+            if (res != null) {
+              result.success(res);
+            } else {
+              result.error("EMPTY", "Cannot fetch active notifications", null);
+            }
+          } else {
+            result.notImplemented();
+          }
+        }
+      );
+  }
+
+  private void createNotificationChannel() {
+    // Create the NotificationChannel, but only on API 26+ because
+    // the NotificationChannel class is new and not in the support library
+    if (VERSION.SDK_INT >= VERSION_CODES.O) {
+      // CharSequence name = getString(R.string.channel_name);
+      // String description = getString(R.string.channel_description);
+      int importance = NotificationManager.IMPORTANCE_DEFAULT;
+      NotificationChannel channel = new NotificationChannel(
+        CHANNEL_ID,
+        "name",
+        importance
+      );
+      channel.setDescription("description");
+      // Register the channel with the system; you can't change the importance
+      // or other notification behaviors after this
+      NotificationManager notificationManager = getSystemService(
+        NotificationManager.class
+      );
+      notificationManager.createNotificationChannel(channel);
+    }
+  }
+
+  Intent resultIntent;
+  PendingIntent pendingIntent;
+  TaskStackBuilder tsb;
+
+  private ArrayList<String> readNotification() {
+    ArrayList<String> l = new ArrayList();
+    if (VERSION.SDK_INT >= VERSION_CODES.M) {
+      StatusBarNotification[] sbn = getSystemService(NotificationManager.class)
+        .getActiveNotifications()
+        .clone();
+      for (StatusBarNotification n : sbn) {
+        l.add(n.getNotification().extras.getString("android.title"));
+      }
+      return l;
+    }
+    return null;
+  }
+
+  private void showNotification(String title, String description, String id) {
+    NotificationCompat.Builder builder = new NotificationCompat.Builder(
+      this,
+      CHANNEL_ID
+    )
+      .setSmallIcon(R.drawable.ic_launcher)
+      .setContentTitle(title)
+      .setContentText(description)
+      .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+    resultIntent = new Intent(this, MainActivity.class);
+    tsb = TaskStackBuilder.create(this);
+    tsb.addParentStack(MainActivity.this);
+
+    tsb.addNextIntent(resultIntent);
+    pendingIntent = tsb.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+    builder.setContentIntent(pendingIntent);
+
+    NotificationManagerCompat notificationManager = NotificationManagerCompat.from(
+      this
+    );
+
+    // notificationId is a unique int for each notification that you must define
+    notificationManager.notify(Integer.parseInt(id), builder.build());
+  }
+
+  private int getBatteryLevel() {
+    int batteryLevel = -1;
+    if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+      BatteryManager batteryManager = (BatteryManager) getSystemService(
+        BATTERY_SERVICE
+      );
+      batteryLevel =
+        batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+    } else {
+      Intent intent = new ContextWrapper(getApplicationContext())
+      .registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+      batteryLevel =
+        (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100) /
+        intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+    }
+
+    return batteryLevel;
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:math';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static const platform = MethodChannel('battery_plugin/channel');
+
+  Future<void> _getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final int result = await platform.invokeMethod('getBatteryLevel');
+      batteryLevel = "Battery level at $result %.";
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    }
+    setState(() {});
+
+    return batteryLevel;
+  }
+
+  Future<void> _showNotification() async {
+    try {
+      final String id = Random().nextInt(10).toString();
+      await platform.invokeMethod('showNotification', {
+        'title': "Topankovo $id",
+        'description': "Nakupte este tento vikend za vyhodne ceny",
+        'id': id
+      });
+    } on PlatformException catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future<void> _readNotifications() async {
+    try {
+      var obj = await platform.invokeMethod('readNotification');
+      print(obj.toString());
+    } on PlatformException catch (e) {
+      print(e.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Battery LEVEL INFO"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Center(
+              child: FutureBuilder(
+            future: _getBatteryLevel(),
+            builder: (context, snapshot) => Text(
+              snapshot.data ?? "Unknown battery level.",
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+          )),
+          FlatButton.icon(
+            icon: Icon(Icons.notifications),
+            onPressed: () => _showNotification(),
+            label: Text("Show Notification"),
+          ),
+          FlatButton.icon(
+            icon: Icon(Icons.notifications),
+            onPressed: () => _readNotifications(),
+            label: Text("Display Notification"),
+          ),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.battery_std,
+          color: Colors.white,
+          size: 40,
+        ),
+        onPressed: () => _getBatteryLevel(),
+      ),
+    );
+  }
+}
